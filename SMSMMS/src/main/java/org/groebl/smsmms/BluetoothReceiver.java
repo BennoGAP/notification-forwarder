a8fe48118d3b064042f89d3c53a424c694c618db
Benno Groebl <mail@groebl.org>
2016-03-25 23:04:30 +0100

    BT-Volume

--- SMSMMS/src/main/java/org/groebl/smsmms/BluetoothReceiver.java
+++ SMSMMS/src/main/java/org/groebl/smsmms/BluetoothReceiver.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 
 import org.groebl.smsmms.transaction.SmsHelper;
