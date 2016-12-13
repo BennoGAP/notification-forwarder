1b2dc499d259b72a7fce0d8af129d3c0b3efc71c
BennoGAP <mail@groebl.org>
2016-12-13 05:24:31 +0100

    Remove check of 2nd Service

--- SMS/src/main/java/org/groebl/sms/BluetoothBootReceiver.java
+++ SMS/src/main/java/org/groebl/sms/BluetoothBootReceiver.java
@@ -10,18 +10,13 @@ public class BluetoothBootReceiver extends BroadcastReceiver {
 
     public void onReceive(Context context, Intent myintent) {
             boolean found = false;
-            boolean found2 = false;
             for (RunningServiceInfo service : ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE)).getRunningServices(Integer.MAX_VALUE)) {
                 if (BluetoothService.class.getName().equals(service.service.getClassName())) {
                     found = true;
-                }
-                else if (BluetoothNotificationService.class.getName().equals(service.service.getClassName())) {
-                    found2 = true;
+                    break;
                 }
             }
 
             if (!found) { context.startService(new Intent(context, BluetoothService.class)); }
-            if (!found2) { context.startService(new Intent(context, BluetoothNotificationService.class)); }
-
     }
 }
