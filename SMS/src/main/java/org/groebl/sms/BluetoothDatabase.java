5c9479dc91fc8fbffa15e2e172d6057c41078d4e
BennoGAP <mail@groebl.org>
2017-01-21 14:03:15 +0100

    Database Exceptions

--- SMS/src/main/java/org/groebl/sms/BluetoothDatabase.java
+++ SMS/src/main/java/org/groebl/sms/BluetoothDatabase.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.util.Log;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,64 +19,65 @@ public class BluetoothDatabase {
 
 
     public static void init(Context context) {
-        SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY, null);
-
-        final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE + " (app STRING(70) NOT NULL, hash STRING(32) NOT NULL, time DATE DEFAULT (datetime('now', 'localtime')) );";
 
         try {
+            SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY | SQLiteDatabase.NO_LOCALIZED_COLLATORS, null);
+            final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE + " (app STRING(70) NOT NULL, hash STRING(32) NOT NULL, time DATE DEFAULT (datetime('now', 'localtime')) );";
             db.execSQL(CREATE_TABLE);
+            if (db != null) db.close();
         } catch (SQLiteException e) {
-            //catch
+            Log.d("SMS", "SQLite error - init");
         }
-        db.close();
-
     }
 
     public static void deleteAll(Context context) {
-        SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY, null);
-        db.delete(TABLE, null, null);
-        db.close();
+
+        try {
+            SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY | SQLiteDatabase.NO_LOCALIZED_COLLATORS, null);
+            db.delete(TABLE, null, null);
+            if (db != null) db.close();
+        } catch (SQLiteException e) {
+            Log.d("SMS", "SQLite error - delete");
+        }
     }
 
     public static void deleteAfterTime(Context context) {
-        SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY, null);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-        Calendar c = Calendar.getInstance();
-        c.setTimeInMillis(System.currentTimeMillis());
-        c.add(Calendar.HOUR_OF_DAY, -12);
-
-        db.execSQL("DELETE from " + TABLE + " WHERE time <= " + "'" + dateFormat.format(c.getTime()) + "';");
-        db.close();
+        try {
+            SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY | SQLiteDatabase.NO_LOCALIZED_COLLATORS, null);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(System.currentTimeMillis());
+            c.add(Calendar.HOUR_OF_DAY, -12);
+            db.execSQL("DELETE from " + TABLE + " WHERE time <= " + "'" + dateFormat.format(c.getTime()) + "';");
+            if (db != null) db.close();
+        } catch (SQLiteException e) {
+            Log.d("SMS", "SQLite error - delete");
+        }
     }
 
     public static boolean searchHash(Context context, String app, String hash) {
-        SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY, null);
 
         new Thread(() -> {
             deleteAfterTime(context);
         }).start();
 
         try {
+            SQLiteDatabase db = context.openOrCreateDatabase(DATABASE, SQLiteDatabase.CREATE_IF_NECESSARY | SQLiteDatabase.NO_LOCALIZED_COLLATORS, null);
+
             //Check if hash is in Database
             Cursor c  = db.rawQuery("select * from " + TABLE + " where app = ? AND hash = ?", new String[]{app, hash});
 
             if (c.getCount() > 0) {
                 //In Database, return true
-                db.close();
+                if (db != null) db.close();
                 return true;
             } else {
                 //Not in Database, so write it in DB and return false
                 ContentValues values = new ContentValues();
                 values.put("app", app);
                 values.put("hash", hash);
-
-                try {
-                    db.insertOrThrow(TABLE, null, values);
-                } catch (Exception e) {
-                    //catch code
-                }
-
-                db.close();
+                db.insertOrThrow(TABLE, null, values);
+                if (db != null) db.close();
                 return false;
             }
 
